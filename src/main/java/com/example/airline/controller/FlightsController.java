249afc3d7ff8a67e79fd977646e060702de1ff14
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-04-18 18:06:44 -0700

    Added sorting feature

--- src/main/java/com/example/airline/controller/FlightsController.java
+++ src/main/java/com/example/airline/controller/FlightsController.java
@@ -17,34 +17,9 @@ public class FlightsController {
     @Autowired
     private FlightService flightService;
 
-//    @GetMapping("/search")
-//    public String searchPage() { return "index"; }
-
-//    @PostMapping("/search")
-//    public String search(@RequestParam("originCity") String originCity, @RequestParam("destCity") String destCity,
-//                         @RequestParam("date") String date, @RequestParam("travelers") String travelers,
-//                         @RequestParam("stops") String stops, Model model) {
-//        model.addAttribute("flights", flightService.query1(originCity, destCity, date, travelers, stops));
-//        model.addAttribute("originCity", originCity);
-//        model.addAttribute("destCity", destCity);
-//        return "search";
-//    }
-//
-//    public String search2() {
-//
-//    }
-
-//    @PostMapping("/searchSort")
-//    public String searchSort(@RequestParam("originCity") String originCity, @RequestParam("destCity") String destCity,
-//                         @RequestParam("date") Integer date, @RequestParam("sorting") Integer sorting, Model model) {
-//        System.out.println(originCity + ", " + destCity+ ", " + date+ ", " + sorting);
-//        //model.addAttribute("flights", flightService.query1(originCity, destCity, date, sorting));
-//        return "search";
-//    }
 
     @PostMapping("/search")
     public String search(FlightQuery flightQuery, Model model) {
-        System.out.println(flightQuery.toString());
         model.addAttribute("flights", flightService.query(flightQuery));
         model.addAttribute("originCity", flightQuery.getOriginCity());
         model.addAttribute("destCity", flightQuery.getDestCity());
@@ -54,7 +29,6 @@ public class FlightsController {
 
     @PostMapping("/searchUpdate")
     public String searchUpdate(FlightQuery flightQuery, Model model) {
-        System.out.println(flightQuery.toString());
         model.addAttribute("flights", flightService.query(flightQuery));
         model.addAttribute("originCity", flightQuery.getOriginCity());
         model.addAttribute("destCity", flightQuery.getDestCity());
