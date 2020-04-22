ea2fb14a3f51f9e248b93a160238222d38361d5e
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-04-22 09:29:24 -0700

    Added Itineraries page

--- src/main/java/com/example/airline/controller/UserController.java
+++ src/main/java/com/example/airline/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Controller
@@ -37,6 +39,12 @@ public class UserController {
         User user = (User) session.getAttribute("user");
         List<Long> fids = orderService.getFidsByUsername(user.getUsername());
         List<Flight> flights = searchService.getFlightsByFid(fids);
+        Collections.sort(flights, new Comparator<Flight>() {
+            @Override
+            public int compare(Flight o1, Flight o2) {
+                return o1.getDay_of_month() - o2.getDay_of_month();
+            }
+        });
         model.addAttribute("flights", flights);
         return "user_order";
     }
